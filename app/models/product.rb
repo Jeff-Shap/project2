ba2cf7420273e3475bbbb280bfc0fd4822c43137
Gabe <gabriel.g.noboa@gmail.com>
2015-05-06 18:55:49 -0400

    initial commit

---
+++ app/models/product.rb
@@ -0,0 +1,20 @@
+class Product < ActiveRecord::Base
+
+	has_many :reviews
+	
+	validates :name, :description, presence: true
+	validates :price_in_cents, numericality: {only_integer: true, greater_than: 0}
+
+	def formatted_price
+		sprintf("%.2f", price_in_dollars)
+	end
+
+	def price_in_dollars=(dollars)
+		self.price_in_cents=(dollars.to_f*100).to_i
+	end
+
+	def price_in_dollars
+		return nil if price_in_cents.nil?
+		price_in_cents/100.0
+	end
+end
