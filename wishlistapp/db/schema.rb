46cdb8d4f7e42abcc1c0987e005283d5545dec7b
Jeff Shapiro <codesheff@gmail.com>
2015-05-06 17:07:17 -0400

    1 category per product

--- wishlistapp/db/schema.rb
+++ wishlistapp/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150504225901) do
+ActiveRecord::Schema.define(version: 20150506210110) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "catname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +25,10 @@ ActiveRecord::Schema.define(version: 20150504225901) do
     t.integer  "price_in_cents"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "category_id"
   end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id"
 
   create_table "reviews", force: :cascade do |t|
     t.text     "comment"
