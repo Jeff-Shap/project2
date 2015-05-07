e784b22b39fa1b688c0cb92d3fec4c9dc0519f79
Jeff Shapiro <codesheff@gmail.com>
2015-05-07 16:01:23 -0400

    added imageurl to product

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506210110) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20150507195806) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "catname"
@@ -29,9 +26,10 @@ ActiveRecord::Schema.define(version: 20150506210110) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "category_id"
+    t.string   "imageurl"
   end
 
-  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+  add_index "products", ["category_id"], name: "index_products_on_category_id"
 
   create_table "reviews", force: :cascade do |t|
     t.text     "comment"
@@ -40,8 +38,6 @@ ActiveRecord::Schema.define(version: 20150506210110) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "reviews", ["product_id"], name: "index_reviews_on_product_id", using: :btree
+  add_index "reviews", ["product_id"], name: "index_reviews_on_product_id"
 
-  add_foreign_key "products", "categories"
-  add_foreign_key "reviews", "products"
 end
